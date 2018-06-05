94ba4e370c0ff27492cedb76e52b768dcf1811c4
WenTao-Love <zhanjiangxuwen@126.com>
2018-06-05 18:46:45 +0800

    update

--- src/test/java/com/szlib/seeker/Test.java
+++ src/test/java/com/szlib/seeker/Test.java
@@ -14,10 +14,10 @@ import cn.hutool.core.util.StrUtil;
 public class Test {
 	
 	public static void main(String[] args) throws IOException {
-		long current = DateUtil.current(false);
-		System.out.println(current);
-		DateTime pdate = DateUtil.parse("2017.12.01", "yyyy.MM");
-		System.out.println(pdate);
+//		long current = DateUtil.current(false);
+//		System.out.println(current);
+//		DateTime pdate = DateUtil.parse("2017.12.01", "yyyy.MM");
+//		System.out.println(pdate);
 //		FileWriter writer = new FileWriter("szlib_test.properties");
 //		writer.write("test");
 //		System.out.println(StrUtil.replaceChars("2015", new char[] {'[',']'}, ""));
@@ -28,5 +28,8 @@ public class Test {
 //		FileWriter writer = new FileWriter(file);
 //		writer.write("test");
 		
+		DateTime dt = DateUtil.parseDate("2018-03-05");
+		DateTime at = DateUtil.offsetDay(dt, 90);
+		System.out.println(at);
 	}
 }
